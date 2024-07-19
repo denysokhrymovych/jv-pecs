e4f25f3e9cc615b64f24f95ef1744926ab1b6f58
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2024-07-19 15:30:40 +0300

    - implement `MachineService` interface - create implementations of `MachineProducer`

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,7 +5,26 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private int loadCapacity;
+    private int numberOfAxles;
+
     public Truck() {
+    }
+
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(int loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    public int getNumberOfAxles() {
+        return numberOfAxles;
+    }
+
+    public void setNumberOfAxles(int numberOfAxles) {
+        this.numberOfAxles = numberOfAxles;
     }
 
     @Override
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,54 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import core.mate.academy.service.producer.BulldozerProducer;
+import core.mate.academy.service.producer.ExcavatorProducer;
+import core.mate.academy.service.producer.MachineProducer;
+import core.mate.academy.service.producer.TruckProducer;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+    private static final MachineProducer<Bulldozer> bulldozerProducer = new BulldozerProducer();
+    private static final MachineProducer<Excavator> excavatorProducer = new ExcavatorProducer();
+    private static final MachineProducer<Truck> truckProducer = new TruckProducer();
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        List<? extends Machine> machines = new ArrayList<>();
+
+        if (type == Bulldozer.class) {
+            machines = bulldozerProducer.get();
+        }
+
+        if (type == Excavator.class) {
+            machines = excavatorProducer.get();
+        }
+
+        if (type == Truck.class) {
+            machines = truckProducer.get();
+        }
+
+        return new ArrayList<>(machines);
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
+    }
 }
